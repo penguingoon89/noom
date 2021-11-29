dfac81ae40f4fe200cc52f210a687d9d3fb3d3f3
penguingoon89 <yhkang@atozsoft.co.kr>
2021-11-29 10:43:40 +0900

    3장 완료 커밋

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,41 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
  
+function makeMessage(type, payload){
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 })
  
 socket.addEventListener("message", (message) => {
-  console.log("Just got this:", message.data, "from the server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 })
  
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 })
+ 
+function handleSubmit(event){
+  event.preventDefault();
+  const input = messageForm.querySelector("input"); 
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+ 
+function handleNickSubmit(event){
+  event.preventDefault();
+  const input = nickForm.querySelector("input"); 
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+ 
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
-setTimeout(() => {
-  socket.send("hello from browser!");
-}, 5000);
--- src/server.js
+++ src/server.js
@@ -14,13 +14,24 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous"
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from Browser"));
-  socket.on("message", (message) => {
-    console.log(`${message}`)
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch(message.type){
+      case "new_message":
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
-  socket.send("hello!!");
 })
 
 server.listen(3000, handleListen);
