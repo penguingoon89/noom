dfc878c28245643529abc97418eef86357474b30
penguingoon89 <yhkang@atozsoft.co.kr>
2021-11-29 10:17:32 +0900

    2장 완료 커밋

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi!");
+const socket = new WebSocket(`ws://${window.location.host}`);
+ 
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+})
+ 
+socket.addEventListener("message", (message) => {
+  console.log("Just got this:", message.data, "from the server");
+})
+ 
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server");
+})
+
+setTimeout(() => {
+  socket.send("hello from browser!");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from 'express';
 
 const app = express();
@@ -9,4 +11,17 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from Browser"));
+  socket.on("message", (message) => {
+    console.log(`${message}`)
+  });
+  socket.send("hello!!");
+})
+
+server.listen(3000, handleListen);
+
